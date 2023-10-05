df6c61541981da266567ca20d23116f56058c8c3
Zhumabek-KG <146426241+Zhumabek-KG@users.noreply.github.com>
2023-10-05 09:26:01 +0600

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -2,7 +2,5 @@ l = [int(i) for i in input().split()]
 for i in range(len(l)):
     for j in range(len(l)):
         if l[i] < l[j]:
-            s = l[j]
-            l[j] = l[i]
-            l[i] = s
+            l[j], l[i] = l[i], l[j]
 print(l)
